07a4cc2059b7e87a27d81658f881a51721d835ed
Ayush Kapur <ayushkapur10@gmail.com>
2017-09-17 01:58:38 -0400

    Changed range

--- src/servotest.py
+++ src/servotest.py
@@ -11,12 +11,12 @@ p.start(7.5)
 
 try:
         while True:
-		p.ChangeDutyCycle(14.5)  # turn towards 90 degree
-		time.sleep(1) # sleep 1 second
-		p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-		time.sleep(1) # sleep 1 second
-		p.ChangeDutyCycle(8.5) # turn towards 180 degree
-                time.sleep(1) # sleep 1 second
+		p.ChangeDutyCycle(17.5)  # turn towards 90 degree
+		time.sleep(0.75) # sleep 1 second
+		#p.ChangeDutyCycle(2.5)  # turn towards 0 degree
+		#time.sleep(0.75) # sleep 1 second
+		p.ChangeDutyCycle(12.5) # turn towards 180 degree
+                time.sleep(0.75) # sleep 1 second
 except KeyboardInterrupt:
 	p.stop()
         GPIO.cleanup()
