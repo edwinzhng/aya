7d80671ecaa88645eb79989ab014ca39e911878a
Edwin Zhang <edwinzhang64@gmail.com>
2017-09-17 08:14:54 -0400

    Update

--- src/moveMouth.py
+++ src/moveMouth.py
@@ -3,9 +3,9 @@ import time
 
 def actuate():
     while True:
-    p.ChangeDutyCycle(25)
-    time.sleep(0.15)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.15)
-    p.ChangeDutyCycle(18)
-    time.sleep(0.15)
+        p.ChangeDutyCycle(25)
+        time.sleep(0.15)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(0.15)
+        p.ChangeDutyCycle(18)
+        time.sleep(0.15)
