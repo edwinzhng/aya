bc67d78e691521bf05d52221c77ffc5a153c230f
Edwin Zhang <edwinzhang64@gmail.com>
2017-09-17 08:14:20 -0400

    Update

--- src/moveMouth.py
+++ src/moveMouth.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 
 def actuate():
-        while True:
-		p.ChangeDutyCycle(25)
-		time.sleep(0.15)
-		p.ChangeDutyCycle(2.5)
-		time.sleep(0.15)
-		p.ChangeDutyCycle(18)
-        time.sleep(0.15)
+    while True:
+    p.ChangeDutyCycle(25)
+    time.sleep(0.15)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.15)
+    p.ChangeDutyCycle(18)
+    time.sleep(0.15)
