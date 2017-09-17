31113a153e5a42dc9b5223fa70ea26c0acf4a89a
Ayush Kapur <ayushkapur10@gmail.com>
2017-09-17 01:51:42 -0400

    Full open close testing, port 18

--- src/servotest.py
+++ src/servotest.py
@@ -11,11 +11,11 @@ p.start(7.5)
 
 try:
         while True:
-		p.ChangeDutyCycle(7.5)  # turn towards 90 degree
+		p.ChangeDutyCycle(14.5)  # turn towards 90 degree
 		time.sleep(1) # sleep 1 second
 		p.ChangeDutyCycle(2.5)  # turn towards 0 degree
 		time.sleep(1) # sleep 1 second
-		p.ChangeDutyCycle(12.5) # turn towards 180 degree
+		p.ChangeDutyCycle(8.5) # turn towards 180 degree
                 time.sleep(1) # sleep 1 second
 except KeyboardInterrupt:
 	p.stop()
